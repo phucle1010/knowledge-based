2e15e395bb981d514c74cbef025abd6c84adda5b
phucle1010 <lethephuc2002@gmail.com>
2026-03-16 10:53:40 +0700

    feat(app): build apis for PATCH, GET and DELETE application

---
+++ types/http.type.ts
@@ -0,0 +1,16 @@
+export type Pagination = {
+    page: number;
+    limit: number;
+};
+
+export type PaginationResponse = {
+    page: number;
+    limit: number;
+    total: number;
+    totalPages: number;
+};
+
+export type ListWithPaginationResponse<T> = {
+    data: T[];
+    meta: PaginationResponse;
+};
