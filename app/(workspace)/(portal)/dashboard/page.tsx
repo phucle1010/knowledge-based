b8d8d385b7bb6027822a2fbf7f9988c498df0d8d
phucle1010 <lethephuc2002@gmail.com>
2026-03-20 16:08:46 +0700

    feat: build global loading component

--- app/(workspace)/(portal)/dashboard/page.tsx
+++ app/(workspace)/(portal)/dashboard/page.tsx
@@ -1,10 +1,12 @@
 import { Suspense } from "react";
 
+import { GlobalLoading } from "@/components/shared/GlobalLoading";
+
 import { Dashboard } from "@/features/dashboard/components";
 
 export default function DashboardPage() {
     return (
-        <Suspense fallback={<div className="w-full h-screen flex items-center justify-center">Loading...</div>}>
+        <Suspense fallback={<GlobalLoading />}>
             <Dashboard />
         </Suspense>
     );
